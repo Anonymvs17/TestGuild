6b06d992d62cdf6514aeb852d328d42b140eacc6
Anonymvs <arapovic.boris@gmail.com>
2024-12-18 20:46:13 +0100

    put password into constants

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -3,12 +3,14 @@ import path from 'path';
 import { LoginPage } from '../pages/LoginPage';
 
 const authFile = path.join(__dirname, '../playwright/.auth/user.json');
+const password = 'secret_sauce';
+const username = 'standard_user';
 
 setup('authenticate', async ({ page }) => {
     const loginPage = new LoginPage(page);
 
     await loginPage.goto();
-    await loginPage.login('standard_user', 'secret_sauce_password');
+    await loginPage.login(username, password);
     // Wait until the page receives the cookies.
     //
     // Sometimes login flow sets cookies in the process of several redirects.
